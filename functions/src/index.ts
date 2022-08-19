741789b7db5d2cbab41c5dc94cdaaed5dd09e949
putonics <putonics@gmail.com>
2022-08-19 14:15:44 +0530

    ROI 4%

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,44 @@
-import { sendEmail } from './classes/email/index';
-import { signup, checkEmail, checkReferer, signin, adminSignin, inactiveUsers, addPackage, load, sendOtp, requestWithdrawal, requestPackage, getWithdrawalList, serveWithdrawal, shareTurnover, getPendingWithdrawals, modify, getPendingPackages, servePackage, modifyAdmin } from './classes/user/index';
-import * as functions from 'firebase-functions'
-import * as firebase from 'firebase-admin'
-import * as express from 'express'
-const cors = require('cors')
+import { sendEmail } from "./classes/email/index"
+import {
+  signup,
+  checkEmail,
+  checkReferer,
+  signin,
+  adminSignin,
+  inactiveUsers,
+  addPackage,
+  load,
+  sendOtp,
+  requestWithdrawal,
+  requestPackage,
+  getWithdrawalList,
+  serveWithdrawal,
+  shareTurnover,
+  getPendingWithdrawals,
+  modify,
+  getPendingPackages,
+  servePackage,
+  modifyAdmin,
+} from "./classes/user/index"
+import * as functions from "firebase-functions"
+import * as firebase from "firebase-admin"
+import * as express from "express"
+const cors = require("cors")
 ////////////////////////////////////////////////////////////////////////////////
 const app = express()
 app.use(cors())
 firebase.initializeApp()
 
-app.get('/test', (req, res) => { res.json({ msg: 'All okay!' }) })
-app.get('/test-email/:email', async (req, res) => {
-    const { email } = req.params
-    const sent = await sendEmail(email, 'Testing Email', '',
-        `
+app.get("/test", (req, res) => {
+  res.json({ msg: "V-1.2.0: All okay!" })
+})
+app.get("/test-email/:email", async (req, res) => {
+  const { email } = req.params
+  const sent = await sendEmail(
+    email,
+    "Testing Email",
+    "",
+    `
         <!DOCTYPE html>
         <html>
             <head>
@@ -24,35 +49,36 @@ app.get('/test-email/:email', async (req, res) => {
                 <h1 style="color:#ff0000;">Just for fun</h1>
             </body>
         </html>
-    `)
-    res.json({ sent })
+    `
+  )
+  res.json({ sent })
 })
 /////////////////////////////////////////////////////////////////////
-app.get('/email/:email', checkEmail)
-app.get('/referer/:refdocid', checkReferer)
+app.get("/email/:email", checkEmail)
+app.get("/referer/:refdocid", checkReferer)
 ////////////////////////////////////////////////////////////////////
-app.post('/user/signup', signup)
-app.post('/user/signin', signin)
-app.post('/user/modify', modify)
-app.post('/user/load', load)
-app.post('/user/inactive', inactiveUsers)
-app.post('/user/package', requestPackage)
-app.post('/user/withdraw', requestWithdrawal)
-app.post('/user/withdrawal-list', getWithdrawalList)//only 10 withdrawal only
+app.post("/user/signup", signup)
+app.post("/user/signin", signin)
+app.post("/user/modify", modify)
+app.post("/user/load", load)
+app.post("/user/inactive", inactiveUsers)
+app.post("/user/package", requestPackage)
+app.post("/user/withdraw", requestWithdrawal)
+app.post("/user/withdrawal-list", getWithdrawalList) //only 10 withdrawal only
 /////////////////////////////////////////////////////////////////////
-app.post('/admin/signin', adminSignin)
-app.post('/admin/modify', modifyAdmin)
-app.post('/admin/package', addPackage)
-app.post('/admin/package-list', getPendingPackages)
-app.post('/admin/package-serve', servePackage)
-app.post('/admin/withdrawal-list', getPendingWithdrawals)
-app.post('/admin/withdraw-serve', serveWithdrawal)
-app.post('/admin/shareturnover', shareTurnover)
+app.post("/admin/signin", adminSignin)
+app.post("/admin/modify", modifyAdmin)
+app.post("/admin/package", addPackage)
+app.post("/admin/package-list", getPendingPackages)
+app.post("/admin/package-serve", servePackage)
+app.post("/admin/withdrawal-list", getPendingWithdrawals)
+app.post("/admin/withdraw-serve", serveWithdrawal)
+app.post("/admin/shareturnover", shareTurnover)
 /////////////////////////////////////////////////////////////////////
-app.post('/send-otp', sendOtp)
+app.post("/send-otp", sendOtp)
 /////////////////////////////////////////////////////////////////////
-app.get('/*', (req, res) => res.status(404).send())
-app.post('/*', (req, res) => res.status(404).send())
+app.get("/*", (req, res) => res.status(404).send())
+app.post("/*", (req, res) => res.status(404).send())
 
 exports.app = functions.https.onRequest(app)
 
