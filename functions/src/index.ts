b811192e4f53685569b71229dd7ddc62bdd5639a
putonics <putonics@gmail.com>
2022-09-18 16:20:11 +0530

    v 1.8.0

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -37,7 +37,7 @@ app.use(cors())
 firebase.initializeApp()
 
 app.get("/test", (req, res) => {
-  res.json({ msg: "V-1.7.0: All okay!" })
+  res.json({ msg: "V-1.8.0: All okay!" })
 })
 app.get("/test-email/:email", async (req, res) => {
   const { email } = req.params
